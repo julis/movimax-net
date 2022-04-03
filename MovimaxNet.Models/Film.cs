12cd900d89801f08cc8cabab9ececfeb1b655210
julis <kangjulis@gmail.com>
2022-04-03 16:15:45 +0700

    create Film model

---
+++ MovimaxNet.Models/Film.cs
@@ -0,0 +1,13 @@
+namespace MovimaxNet.Models;
+
+public class Film
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public uint RuntimeMinute { get; set; }
+    public string Revenue { get; set; }
+    public string PosterUrl { get; set; }
+    public ICollection<Genre> Genres { get; set; }
+}
