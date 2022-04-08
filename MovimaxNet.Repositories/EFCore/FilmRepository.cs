feb6798488ac9c91d8eb0d6ea37d164f14b57e3a
julis <kangjulis@gmail.com>
2022-04-09 01:54:16 +0700

    create ef core film repository

---
+++ MovimaxNet.Repositories/EFCore/FilmRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovimaxNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovimaxNet.Repositories.EFCore
+{
+    internal class FilmRepository : RepositoryBase<Film>, IFilmRepository
+    {
+        public FilmRepository(DatabaseContext context) : base(context)
+        {
+        }
+
+        public override async Task<IEnumerable<Film>> GetAll()
+        {
+            try
+            {
+                return await context.Films.Include(f => f.FilmGenres).ToListAsync();                
+            }
+            catch (Exception ex)
+            {
+                return new List<Film>();
+            }
+        }
+    }
+}
