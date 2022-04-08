9f0e54e2bf34302ee24d05f518728f042873debb
julis <kangjulis@gmail.com>
2022-04-08 21:56:05 +0700

    change films property to optional and add relation to the filmgenre

--- MovimaxNet.Models/Film.cs
+++ MovimaxNet.Models/Film.cs
@@ -3,11 +3,11 @@ namespace MovimaxNet.Models;
 public class Film : IModel
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
     public DateTime ReleaseDate { get; set; }
     public uint RuntimeMinute { get; set; }
-    public string Revenue { get; set; }
-    public string PosterUrl { get; set; }
-    public ICollection<Genre> Genres { get; set; }
+    public string? Revenue { get; set; }
+    public string? PosterUrl { get; set; }
+    public IList<FilmGenre>? FilmGenres { get; set; }
 }
