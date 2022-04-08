979cd1a05495a625f1ae2ed09444161a5e89531c
julis <kangjulis@gmail.com>
2022-04-09 01:57:24 +0700

    regiester database context and repository service to the programs

--- MovimaxNet.WebApi/Program.cs
+++ MovimaxNet.WebApi/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MovimaxNet.Repositories;
+using MovimaxNet.Repositories.EFCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DatabaseContext>(option => 
+    option.UseSqlite("Data Source=database.db")
+);
+builder.Services.AddScoped<IRepositoryService, RepositoryService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
